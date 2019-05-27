9ad93cdeeb06c01a877f9f59f3821b7cfcea44f9
James Vickery <dev@jamesvickery.net>
2019-05-27 20:36:13 +0100

    dont commit styling changes blind

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,18 +3,20 @@ import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <div className="container" style={{ margin: '2em 0' }}>
-      <h1>
-        404{' '}
-        <span role="img" aria-label="404 face">
-          🙄
-        </span>
-      </h1>
-      <p>*tumbleweed*</p>
-      <p>
-        if you think there's supposed to be something here, please heckle me on{' '}
-        <a href="https://twitter.com/jmsve">twitter</a> or something
-      </p>
+    <div className="container">
+      <div style={{ margin: '2em 0' }}>
+        <h1>
+          404{' '}
+          <span role="img" aria-label="404 face">
+            🙄
+          </span>
+        </h1>
+        <p>*tumbleweed*</p>
+        <p>
+          if you think there's supposed to be something here, please heckle me on{' '}
+          <a href="https://twitter.com/jmsve">twitter</a> or something
+        </p>
+      </div>
     </div>
   </Layout>
 )

2962a71ae68d75b57724ea3598bf3a3847098326
James Vickery <dev@jamesvickery.net>
2019-07-04 08:18:45 +0100

    DiagonalSplit refactor

--- src/components/DiagonalSplit.js
+++ src/components/DiagonalSplit.js
@@ -1,22 +1,20 @@
 import React from 'react'
 
 export default function DiagonalSplit(props) {
+  const color = props.color || 'white'
+  const polygonPoints = !props.invert ? '1, 0 0, 1 1, 1' : '0, 1 1, 0 0, 0'
+
   return (
     <svg
       style={{
         display: 'block',
         width: '100%',
-        height: 'calc(40px + 5vw)',
+        height: 'calc(30px + 5vw)',
       }}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 1 1"
-      preserveAspectRatio="none"
-    >
-      {!props.invert ? (
-        <polygon points="1, 0 0, 1 1, 1" fill={props.color || 'white'} />
-      ) : (
-        <polygon points="0, 1 1, 0 0, 0" fill={props.color || 'white'} />
-      )}
+      preserveAspectRatio="none">
+      <polygon points={polygonPoints} fill={color} />
     </svg>
   )
 }

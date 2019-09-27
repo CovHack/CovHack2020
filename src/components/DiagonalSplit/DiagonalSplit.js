f45e438bb9ced9e65deca361af233b38c3d55481
James Vickery <dev@jamesvickery.net>
2019-09-27 10:46:51 +0100

    firefox sucks

--- src/components/DiagonalSplit/DiagonalSplit.js
+++ src/components/DiagonalSplit/DiagonalSplit.js
@@ -7,16 +7,18 @@ export const DiagonalSplit = props => {
   const polygonPoints = !props.invert ? '1, 0 0, 1 1, 1' : '0, 1 1, 0 0, 0'
 
   return (
-    <svg
-      style={{
-        display: 'block',
-        width: '100%',
-        height: 'calc(30px + 5vw)',
-      }}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 1 1"
-      preserveAspectRatio="none">
-      <polygon points={polygonPoints} fill={color} />
-    </svg>
+    <div className="split-container">
+      <svg
+        style={{
+          display: 'block',
+          width: '100%',
+          height: 'calc(30px + 5vw)',
+        }}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1 1"
+        preserveAspectRatio="none">
+        <polygon points={polygonPoints} fill={color} />
+      </svg>
+    </div>
   )
 }

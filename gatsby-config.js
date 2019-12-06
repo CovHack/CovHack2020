f8ef3f7e364cca1293aae52c9a02df1a28b9f854
bahorn <b@horn.uk>
2019-12-06 00:33:19 +0000

    Removed GA and replaced map with Leaflet

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,18 @@ module.exports = {
   siteMetadata: {
     title: 'CovHack2020',
     description: `CovHackSoc's Second Annual Hackathon`,
+    map: {
+      url: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      id: 'mapbox/streets-v11',
+      accessToken: 'pk.eyJ1IjoiY292aGFjayIsImEiOiJjazN0YzdoZ3owMWF6M25xOHg5OGhweGZzIn0.D9GaXpt0un2dtWm-NlVt7A',
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      location: {
+        position: [52.40526, -1.49989],
+        name: 'Engineering & Computing Building, Coventry University',
+        description: '3 Gulson Rd, Coventry CV1 2JH, United Kingdom'
+      }
+
+    }
   },
   plugins: [
     'gatsby-plugin-netlify-cms',
@@ -44,11 +56,5 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-148892494-1',
-      },
-    },
   ],
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,14 +1,67 @@
 import React from 'react'
-
+import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet'
+import { graphql, useStaticQuery } from 'gatsby'
 import './Map.scss'
+import 'leaflet/dist/leaflet.css'
 
-export const Map = () => (
-  <div className="map-responsive mt-5 mb-5">
-    <iframe
-      title="CovHack Map"
-      src="https://maps.google.com/maps?q=Engineering%20%26%20Computing%20Building%2C%20Coventry%20University&t=&z=13&ie=UTF8&iwloc=&output=embed"
-      scrolling="no"
-      frameBorder="0"
-    />
-  </div>
-)
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+
+const AlwaysOpenMarker = props => {
+  const initMarker = ref => {
+    if (ref) {
+      ref.leafletElement.openPopup()
+    }
+  }
+  return <Marker ref={initMarker} {...props}/>
+}
+
+export const Map = () => {
+  const {accessToken, url, id, attribution, location} = useStaticQuery(mapQuery).site.siteMetadata.map
+  return (
+    <div className="map-responsive mt-5 mb-5">
+      <LeafletMap className="leaflet-map"center={location.position} zoom={16}>
+        <TileLayer
+          url={url}
+          id={id}
+          accessToken={accessToken}
+          attribution={attribution}
+          />
+          <AlwaysOpenMarker position={location.position}>
+            <Popup>
+              <b>{location.name}</b>
+              <br/>{location.description}
+            </Popup>
+          </AlwaysOpenMarker>
+      </LeafletMap>
+    </div>
+  );
+}
+
+const mapQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        map {
+          url
+          id
+          accessToken
+          attribution
+          location {
+            name
+            position
+            description
+          }
+        }
+      }
+    }
+  }
+`

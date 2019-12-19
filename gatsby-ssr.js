2ab27f9b886621d3b1a974c7c6918708fdbd4049
James Vickery <dev@jamesvickery.net>
2019-12-19 13:56:47 +0000

    gatsby ssr formatting

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,15 @@
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-    if (process.env.NODE_ENV !== 'production')
-        return;
+  if (process.env.NODE_ENV !== 'production') return
 
-    let hc = getHeadComponents();
-    hc.forEach(el => {
-        if (el.type === 'style') {
-            el.type = 'link';
-            el.props['href'] = el.props['data-href'];
-            el.props['rel'] = 'stylesheet';
-            el.props['type'] = 'text/css';
+  getHeadComponents().forEach(el => {
+    if (el.type === 'style') {
+      el.type = 'link'
+      el.props.href = el.props['data-href']
+      el.props.rel = 'stylesheet'
+      el.props.type = 'text/css'
 
-            delete el.props['data-href'];
-            delete el.props['dangerouslySetInnerHTML'];
-        }
-    })
+      delete el.props['data-href']
+      delete el.props.dangerouslySetInnerHTML
+    }
+  })
 }

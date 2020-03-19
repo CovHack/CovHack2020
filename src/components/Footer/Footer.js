f9b2b230fdc798cd993b1d8bdfee0a3e3a8db774
James Vickery <dev@jamesvickery.net>
2020-03-19 15:46:21 +0000

    edit footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,15 +13,17 @@ export const Footer = () => (
     <DiagonalSplit invert />
     <div className="footer-inner">
       <div className="love">
-        Made with <FaHeart /> by <a href="https://www.cusu.org/society/hackathon/">CovHackSoc</a>
+        Made with <FaHeart /> and GatsbyJS by{' '}
+        <a href="https://www.cusu.org/society/hackathon/">CovHackSoc</a> — Edit this site on{' '}
+        <a href="https://github.com/CovHack/CovHack2020">GitHub</a>
       </div>
+
       <div>
         <div>
           <FaTwitter /> <a href="https://twitter.com/covhack">@covhack</a>
           {' / '}
           <a href="https://twitter.com/covhacksoc">@covhacksoc</a>
-        </div>
-        <div>
+          {' / '}
           <MdEmail /> <a href="mailto:hello@covhack.org">hello@covhack.org</a>
         </div>
       </div>
